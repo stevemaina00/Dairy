9bddb616332203788a6bf53b405fe1a2b4d6d649
stevemaina00 <steve.maina@student.moringaschool.com>
2022-07-18 08:39:20 +0300

    finalcommit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const btn=document.getElementById("Results")
 btn.addEventListener("click",(event)=>{
     event.preventDefault()
-let shedA=document.getElementById("inputA").value
-let shedB=document.getElementById("inputB").value
-let shedC=document.getElementById("inputC").value
-let shedD=document.getElementById("inputD").value
+let inputA=document.getElementById("inputA").value
+let inputB=document.getElementById("inputB").value
+let inputC=document.getElementById("inputC").value
+let inputD=document.getElementById("inputD").value
 let days=document.getElementById("days").value
 let totals=totalProduce(inputA,inputB,inputC,inputD);
 incomeOverTime (45,days,totals)
